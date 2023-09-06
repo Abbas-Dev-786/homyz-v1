eb4e1e3a7519a18565022fd52c8fc97ed97370d9
Abbas-Dev-786 <abbasbhanpura.dev@gmail.com>
2023-09-06 20:08:38 +0530

    updated cors

--- server/app.js
+++ server/app.js
@@ -23,7 +23,7 @@ app.enable("trust proxy");
 const whitelist = [
   "http://localhost:5173",
   "https://homyz-amb.netlify.app",
-  "https://dashboard.stripe.com/",
+  "https://api.stripe.com/v1/webhook_endpoints",
 ];
 const corsOptions = {
   origin: (origin, callback) => {
